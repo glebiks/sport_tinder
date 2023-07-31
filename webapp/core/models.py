c9da31b960213260c068a732cbe71ff180d7e86d
Глеб Рахимжанов <cubicscience@yandex.ru>
2023-08-01 01:40:12 +0300

    added edit profile, no sport field

--- webapp/core/models.py
+++ webapp/core/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django import forms
 
 User = get_user_model()
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    profileimg = models.ImageField(
+        upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
     SPORTS = (
@@ -22,8 +26,12 @@ class Profile(models.Model):
         (8, 'boxing'),
         (9, 'chess'),
     )
+
     sport = models.IntegerField(verbose_name='sport', choices=SPORTS, null=True, blank=True, default=1)
 
     # преобразование к типу строки, для вывода в шаблоны и бд
     def __str___(self):
         return self.user.username
+    
+class SportsForm(forms.Form):
+    sport = forms.ChoiceField(choices = Profile.SPORTS)
--- webapp/core/views.py
+++ webapp/core/views.py
@@ -2,13 +2,54 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, SportsForm
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_profile = Profile.objects.get(user=request.user)
+    context = {'user_profile': user_profile}
+    return render(request, 'index.html', context)
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    context = {'user_profile': user_profile}
+    context['form'] = SportsForm
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            # sport = SportsForm(request.POST)
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            # user_profile.sport = sport
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            # sport = SportsForm(request.POST)
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            # user_profile.sport = sport
+            user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'setting.html', context)
+
 
 def signup(request):
     if request.method == 'POST':
@@ -22,22 +63,28 @@ def signup(request):
                 messages.info(request, 'Email already exists')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
                 user.save()
 
                 # Log in user and redirect to settings page
+                user_login = auth.authenticate(
+                    username=username, password=password)
+                auth.login(request, user_login)
 
                 # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user = user_model.id)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signin')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
     else:
         return render(request, 'signup.html')
-    
+
+
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -51,10 +98,11 @@ def signin(request):
         else:
             messages.info(request, 'Username or Password is Invalid')
             return redirect('signin')
-        
+
     else:
         return render(request, 'signin.html')
-    
+
+
 @login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
